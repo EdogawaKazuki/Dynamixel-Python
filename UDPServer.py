6834ce251ba24a11c03556f6cb01a0c6180869fd
edogawakazuki <galad.zhao@gmail.com>
2024-08-07 18:25:21 +0800

    - added head tracking

--- UDPServer.py
+++ UDPServer.py
@@ -2,7 +2,6 @@ import socket
 import struct
 import threading
 import time
-import DynamixelArmClient
 
 
 def bytes_to_float(byte_array, byte_order='<'):
@@ -25,41 +24,49 @@ class UDPServer:
         self.client_addr = None
         self.is_connected = False
         self.send_thread = None
+        self.cmd_callback = None
 
     def start_server(self):
         self.socket.bind((self.host, self.port))
         print(f"UDP receiver started on {self.host}:{self.port}")
         data, addr = self.socket.recvfrom(1024)  # Adjust buffer size as needed
         while True:
-            data, addr = self.socket.recvfrom(1024)  # Adjust buffer size as needed
-            cmd_type = data[0]
-            if cmd_type == 0:
-                print("Connect Message: ")
-                cmd_data = data[1]
-                if cmd_data == 1:
-                    print("Connect")
-                    self.send_thread = threading.Thread(target=self.start_sender)
-                    self.send_thread.start()
-                    self.is_connected = True
-                    self.client_addr = addr
-                else:
-                    print("Disconnect")
-                    self.is_connected = False
-                    self.send_thread.join()
-            elif cmd_type == 1:
-                print("Unlock Message: ")
-                cmd_data = data[1]
-                if cmd_data == 0:
-                    print("Unlock")
-                else:
-                    print("Lock")
-            elif cmd_type == 2:
-                print("Angle Command")
-                # print(data)
-                for i in range(3):
-                    # print(data[1 + i * 4: 1 + (i + 1) * 4])
-                    print(f"Angle {i}: {bytes_to_float(data[1 + i * 4: 1 + (i + 1) * 4], '<')}", end="; ")
-                print()
+            try:
+                data, addr = self.socket.recvfrom(1024)  # Adjust buffer size as needed
+                cmd_type = data[0]
+                if cmd_type == 0:
+                    print("Connect Message: ")
+                    cmd_data = data[1]
+                    if cmd_data == 1:
+                        print("Connect")
+                        self.send_thread = threading.Thread(target=self.start_sender)
+                        self.send_thread.start()
+                        self.is_connected = True
+                        self.client_addr = addr
+                    else:
+                        print("Disconnect")
+                        self.is_connected = False
+                        self.send_thread.join()
+                elif cmd_type == 1:
+                    print("Unlock Message: ")
+                    cmd_data = data[1]
+                    if cmd_data == 0:
+                        print("Unlock")
+                    else:
+                        print("Lock")
+                elif cmd_type == 2:
+                    print("Angle Command")
+                    # print(data)
+                    angles = []
+                    for i in range(3):
+                        # print(data[1 + i * 4: 1 + (i + 1) * 4])
+                        angles.append(bytes_to_float(data[1 + i * 4: 1 + (i + 1) * 4], '<'))
+                        print(f"Angle {i}: {angles[i]}", end="; ")
+
+                    self.cmd_callback(angles)
+                    print()
+            except Exception as e:
+                print(e)
 
     def start_sender(self):
         while True:
--- test.py
+++
@@ -1,8 +0,0 @@
-import socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('0.0.0.0', 22000))
-print(f"UDP receiver started on {'0.0.0.0'}:{22000}")
-data, addr = server_socket.recvfrom(1024)  # Adjust buffer size as needed
-while True:
-    data, xaddr = server_socket.recvfrom(1024)  # Adjust buffer size as needed
-    print(data)
